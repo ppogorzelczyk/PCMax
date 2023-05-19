8684eb66c602fd39be65780fb5ca9877df3ee6dc
ppogorzelczyk <patryk.pogorzelczyk@gmail.com>
2023-05-19 22:00:52 +0200

    upgrade displaying computation

--- PCMax/Program.cs
+++ PCMax/Program.cs
@@ -5,31 +5,34 @@ class Program
     static void Main()
     {
         var fileReader = new PCMaxFileReader();
-        var scheduler = new GeneticTaskScheduler();
 
         // Process the file and display the results
-        const string filePath = "./m10n200.txt";
-        ReadFileAndCompute(fileReader, filePath, scheduler);
+        const string filePath = "./m50n1000.txt";
+        ReadFileAndCompute(fileReader, filePath);
 
         Console.ReadLine();
     }
 
-    private static void ReadFileAndCompute(PCMaxFileReader fileReader, string filePath, GeneticTaskScheduler scheduler)
+    private static void ReadFileAndCompute(PCMaxFileReader fileReader, string filePath)
     {
-        Console.WriteLine("Processing the file:");
-        try
-        {
-            var fileValues = fileReader.ReadFile(filePath);
-            ConsoleOutput.DisplayGeneratedInstance(fileValues);
-            var completionTimes = scheduler.ScheduleTasks(fileValues);
-            var totalCompletionTime = scheduler.GetTotalCompletionTime(completionTimes);
+        Console.WriteLine($"Processing the file ({filePath}):");
+        var scheduler = new GeneticTaskScheduler();
+        var fileValues = fileReader.ReadFile(filePath);
+        ConsoleOutput.DisplayGeneratedInstance(fileValues);
 
-            ConsoleOutput.DisplayTaskScheduling(completionTimes);
-            ConsoleOutput.DisplayTotalCompletionTime(totalCompletionTime);
-        }
-        catch (Exception ex)
-        {
-            Console.WriteLine("Error: " + ex.Message);
-        }
+        Console.WriteLine($"[{DateTime.Now}] Generating best result with greedy algorithm:");
+        var greedy = new GreedyTaskScheduler();
+        var greedyCompletionTimes = greedy.ScheduleTasks(fileValues);
+        var greedyTotalCompletionTime = scheduler.GetTotalCompletionTime(greedyCompletionTimes);
+
+        ConsoleOutput.DisplayTaskScheduling(greedyCompletionTimes);
+        ConsoleOutput.DisplayTotalCompletionTime(greedyTotalCompletionTime);
+
+        Console.WriteLine($"[{DateTime.Now}] Generating best result with genetic algorithm:");
+        var completionTimes = scheduler.ScheduleTasks(fileValues);
+        var totalCompletionTime = scheduler.GetTotalCompletionTime(completionTimes);
+
+        ConsoleOutput.DisplayTaskScheduling(completionTimes);
+        ConsoleOutput.DisplayTotalCompletionTime(totalCompletionTime);
     }
 }
